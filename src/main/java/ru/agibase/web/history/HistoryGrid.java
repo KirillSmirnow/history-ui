651e114cd526a87d5e668803609359704f769034
KirillSmirnov <smirnowku@gmail.com>
2025-01-26 18:01:39 +0300

    qp

--- src/main/java/ru/agibase/web/history/HistoryGrid.java
+++ src/main/java/ru/agibase/web/history/HistoryGrid.java
@@ -1,10 +1,12 @@
 package ru.agibase.web.history;
 
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
 import lombok.RequiredArgsConstructor;
@@ -21,22 +23,38 @@ public class HistoryGrid extends VerticalLayout {
 
     @Override
     protected void onAttach(AttachEvent attachEvent) {
-        var result = participationHistoryClient.search(1L, null, 0, 10);
+        setWidth("1200px");
+
+        UI.getCurrent().getPage().fetchCurrentURL(url -> {
+            QueryParameters.fromString(url.getQuery()).getSingleParameter("sportsmanId").ifPresent(sportsmanId -> {
+                refresh(Long.valueOf(sportsmanId));
+            });
+        });
+    }
+
+    private void refresh(Long sportsmanId) {
+        var result = participationHistoryClient.search(sportsmanId, null, 0, 10);
         result.getContent().forEach(competition -> {
             var grid = new Grid<>(RatingResult.class, false);
             grid.addColumn("name").setHeader("Зачёт");
             grid.addColumn("dog.homeName").setHeader("Собака");
             grid.addColumn("result.timeInSeconds").setHeader("Сумма времени");
             grid.addColumn("result.totalFaults").setHeader("Сумма штрафов");
+            grid.addColumn("result.place").setHeader("Место");
             grid.setItemDetailsRenderer(new ComponentRenderer<>(CourseResultComponent::new, CourseResultComponent::setItems));
+            grid.setHeight("200px");
             grid.setItems(competition.getRatings());
 
             var nameField = new TextField(null, competition.getName(), (String) null);
             nameField.setWidthFull();
+            nameField.setReadOnly(true);
             var venueField = new TextField(null, competition.getVenue().getName(), (String) null);
             venueField.setWidthFull();
+            venueField.setReadOnly(true);
 
-            add(new VerticalLayout(nameField, venueField, grid));
+            var layout = new VerticalLayout(nameField, venueField, grid);
+            layout.setSpacing(false);
+            add(layout);
         });
     }
 
--- src/main/java/ru/agibase/web/history/model/Result.java
+++ src/main/java/ru/agibase/web/history/model/Result.java
@@ -6,8 +6,36 @@ import java.math.BigDecimal;
 
 @Data
 public class Result {
+
     private final BigDecimal timeInSeconds;
     private final BigDecimal timeFaults;
     private final BigDecimal courseFaults;
     private final BigDecimal totalFaults;
+
+    private final Integer place;
+    private final boolean eliminated;
+    private final boolean absent;
+    private final boolean disqualified;
+    private final boolean unrated;
+
+    public String getPlace() {
+        var builder = new StringBuilder();
+        if (disqualified) {
+            builder.append("Дисквал.");
+        } else if (eliminated) {
+            builder.append("Снят");
+        } else if (absent) {
+            builder.append("Неявка");
+        } else if (place != null) {
+            builder.append(place);
+        }
+        if (unrated) {
+            if (builder.isEmpty()) {
+                builder.append("Вне зачёта");
+            } else {
+                builder.append(" (Вне зачёта)");
+            }
+        }
+        return builder.toString();
+    }
 }
